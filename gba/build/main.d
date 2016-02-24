e747614842b0c1f6926aaa1a2b8a2b29d641032e
Noah Rosamilia <ivoahivoah@gmail.com>
2016-02-23 23:51:24 -0500

    Misc GBA stuff

--- gba/build/main.d
+++ gba/build/main.d
@@ -5,6 +5,7 @@ main.o: /Users/ivo/Desktop/Programming/nCHIP-8/gba/source/main.c \
  /Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_interrupt.h \
  /Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_systemcalls.h \
  /Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_input.h \
+ /Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_dma.h \
  /Users/ivo/Desktop/Programming/nCHIP-8/gba/source/../../core/chip8.h
 
 /Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_video.h:
@@ -18,5 +19,7 @@ main.o: /Users/ivo/Desktop/Programming/nCHIP-8/gba/source/main.c \
 /Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_systemcalls.h:
 
 /Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_input.h:
+
+/Users/ivo/Desktop/Programming/GBA/devkitPro/libgba/include/gba_dma.h:
 
 /Users/ivo/Desktop/Programming/nCHIP-8/gba/source/../../core/chip8.h:
--- gba/source/main.c
+++ gba/source/main.c
@@ -2,10 +2,13 @@
 #include <gba_interrupt.h>
 #include <gba_systemcalls.h>
 #include <gba_input.h>
+#include <gba_dma.h>
 
 #include "../../core/chip8.h"
 
-#define SCALE			3
+#define SCALE		3
+#define X_OFFSET	24
+#define Y_OFFSET	32
 
 void setPixel(int x, int y, int size, u16 color);
 //extern uint8_t IBMLogo_ch8[];
@@ -20,16 +23,32 @@ const uint8_t keymap[] = {5, //KEY_A
 						  0, //KEY_START
 						  6, //KEY_RIGHT
 						  4, //KEY_LEFT
-						  0, //KEY_UP
+						  5, //KEY_UP
 						  0, //KEY_DOWN
-						  0, //KEY_R
-						  0, //KEY_L
+						  6, //KEY_R
+						  4, //KEY_L
 };
+
+typedef struct DMA_REC
+{
+    const void *src;
+    void *dst;
+    u32 cnt;
+} DMA_REC;
+
+#define REG_DMA ((volatile DMA_REC*)0x040000B0)
+
+inline void dma3_fill(void *dst, volatile u32 src, uint count) {
+	REG_DMA[3].cnt = 0; // shut off any previous transfer
+	REG_DMA[3].src = (const void*)&src;
+	REG_DMA[3].dst = dst;
+	REG_DMA[3].cnt = count | DMA_SRC_FIXED | DMA_ENABLE;
+}
+
 //---------------------------------------------------------------------------------
 // Program entry point
 //---------------------------------------------------------------------------------
 int main(void) {
-//---------------------------------------------------------------------------------
 
 	REG_DISPCNT = MODE_3 | BG2_ENABLE;
 
@@ -43,6 +62,8 @@ int main(void) {
 	irqInit();
 	irqEnable(IRQ_VBLANK);
 
+	dma3_fill((void*)VRAM, 0x0000, SCREEN_WIDTH * SCREEN_HEIGHT);
+
 	while (1) {
 		for (int i = 0; i < 25; i++) {
 			chip8_step(&emu);
@@ -52,9 +73,9 @@ int main(void) {
 			for (int y = 0; y < 32; y++) {
 				for (int x = 0; x < 64; x++) {
 					if (emu.gfx[(y*64) + x]) {
-						setPixel(x*SCALE, y*SCALE, SCALE, 0xFFFF);
+						setPixel(x*SCALE + X_OFFSET, y*SCALE + Y_OFFSET, SCALE, 0xFFFF);
 					} else {
-						setPixel(x*SCALE, y*SCALE, SCALE, 0x0000);
+						setPixel(x*SCALE + X_OFFSET, y*SCALE + Y_OFFSET, SCALE, 0x0000);
 					}
 				}
 			}
@@ -66,7 +87,6 @@ int main(void) {
 		for (int i = 0; i <= 9; i++) {
 			emu.keys[keymap[i]] = keys & (1<<i);
 		}
-
 
 		VBlankIntrWait();
 	}
